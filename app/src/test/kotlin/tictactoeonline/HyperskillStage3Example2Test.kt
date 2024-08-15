267d1047f0fb6c70e9b5f465dd5c454d0e509684
John G. Kroubalkian <javapda@yahoo.com>
2024-08-15 16:06:44 -0700

    completed testing of Example 2 step 10, steps 11-12 remaining

--- app/src/test/kotlin/tictactoeonline/HyperskillStage3Example2Test.kt
+++ app/src/test/kotlin/tictactoeonline/HyperskillStage3Example2Test.kt
@@ -169,6 +169,22 @@ class HyperskillStage3Example2Test {
                 )
             }
 
+            // 10. Request: POST /game/1/move
+            // auth Artem (who will be playing as both Player1 and Player2) move (2,2) - Success
+            handleRequest(HttpMethod.Post, "/game/1/move") {
+                addHeader(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+                addHeader(HttpHeaders.Authorization, "Bearer ${user1.jwt}")
+                val json = Json.encodeToString(PlayerMoveRequestPayload("(2,2)"))
+                setBody(json)
+            }.apply {
+                assertEquals(Status.MOVE_DONE.statusCode, response.status())
+                assertEquals(
+                    Status.MOVE_DONE.message,
+                    Json.decodeFromString<PlayerMoveResponsePayload>(response.content.toString()).status
+                )
+            }
+
+
         }
     }
 }
