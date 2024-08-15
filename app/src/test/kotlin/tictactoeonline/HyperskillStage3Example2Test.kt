6570fb1f2aad44703288d9852000f4537d8d5fec
John G. Kroubalkian <javapda@yahoo.com>
2024-08-15 15:50:43 -0700

    completed testing of Example 1 step 1, steps 2-12 remaining

---
+++ app/src/test/kotlin/tictactoeonline/HyperskillStage3Example2Test.kt
@@ -0,0 +1,46 @@
+package tictactoeonline
+
+import com.auth0.jwt.JWT
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.server.testing.*
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class HyperskillStage3Example2Test {
+    fun emailFromJwt(jwt: String) =
+        JWT.require(algorithm).build().verify(jwt).getClaim("email").asString()
+
+    @BeforeEach
+    fun setup() {
+        clearAll()
+    }
+
+    @Test
+    fun `Example 2`() {
+        withTestApplication(Application::module) {
+            lateinit var user1: User
+//            lateinit var user2: User
+//            lateinit var response: HttpResponse
+            val example2Size = "3x3"
+
+            // 1. Request: POST /signup
+            // signup Artem without password - failure
+            handleRequest(HttpMethod.Post, "/signup") {
+                addHeader(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+                user1 = User(email = emailArtem, password = "")
+                val json = Json.encodeToString(user1)
+                setBody(json)
+            }.apply {
+                val expectedOnFirstSignup = Json.encodeToString(mapOf("status" to Status.REGISTRATION_FAILED.message))
+                assertEquals(expectedOnFirstSignup, response.content.toString())
+                assertEquals(0, info().num_users)
+
+            }
+
+        }
+    }
+}
